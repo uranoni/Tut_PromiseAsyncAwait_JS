5eb873b0dc2ec5bbb53f1a9fec293389b4adc624
uranoni <uranoni777@gmail.com>
2023-11-09 01:42:09 +0800

    Update promise.js
    add original function

--- promise.js
+++ promise.js
@@ -1,3 +1,18 @@
+//沒有使用promise 的情況
+function main1() {
+    console.log("初始值\n", Date())
+    setTimeout(() => {
+        console.log(`執行第一次 花5秒的結果${Date()}`)
+        Date()
+    }, 5000)
+
+    setTimeout(() => {
+        console.log(`執行第二次 花2秒的結果${Date()}`)
+        
+    }, 2000)
+
+}
+main1()
 
 function count(val, time) {
     return new Promise((resolve, reject) => {
@@ -8,18 +23,19 @@ function count(val, time) {
     })
 }
 // 這邊是main function 使用promise chain來串連執行
-function main() {
-    console.log("初始值\n", Date())
-    count('第一個時間', 3000)
-        .then(val => {
-            console.log(val)
-            return count('第二個時間', 5000)
-        }).then(val => {
-            console.log(val)
-        })
+// function main() {
+//     console.log("初始值\n", Date())
+//     count('第一個時間', 3000)
+//         .then(val => {
+//             console.log(val)
+//             return count('第二個時間', 5000)
+//         }).then(val => {
+//             console.log(val)
+//         })
 
-}
-main()
+// }
+// main()
+
 
 
 // 這邊是使用promise all執行結果
@@ -30,6 +46,3 @@ main()
 //     })
 // }
 // main2()
-
-
-
